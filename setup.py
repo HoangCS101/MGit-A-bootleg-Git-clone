3d77f2f30a0fe28592696e04b55100bd345c29bf
HoangCS101 <hoangcs101@gmail.com>
2025-07-29 22:39:20 +0700

    comments + argparse walkthru

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+# shebang: Allow the script to be run as an executable, using Python 3 as the interpreter (Unix-like systems).
+# setup.py: Script to set up the ugit package.
 
 from setuptools import setup
+# setuptools: A Python package used for distributing Python packages.
 
-setup (name = 'ugit',
+setup (name = 'ugit', # Name for distribution.
        version = '1.0',
-       packages = ['ugit'],
+       packages = ['ugit'], # Code folders to include
        entry_points = {
            'console_scripts' : [
                'ugit = ugit.cli:main'
--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,24 @@
+import argparse
+# Python built-in module for parsing command-line arguments.
+
 def main():
-    print ('Hello, World')
+    args = parse_args()
+    args.func(args)
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # Create a new ArgumentParser object.
+    
+    commands = parser.add_subparsers(dest='command', required=True)
+    # Add subparsers for different commands (like 'init', 'commit',...)
+    # the 'dest' argument specifies the attribute name for the command.
+    # and make the command argument required.
+    
+    commands.add_parser('init').set_defaults(func=init)
+    
+    return parser.parse_args()
+    # This should return Namespace(command='init', func=<function 'init' below>) for 'ugit init'
+
+
+def init(args):
+    print("Initializing ugit repository...")
