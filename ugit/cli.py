c1861ae1e4a3ca75a5f12a36d4c46fa7c68c05ba
HoangCS101 <hoangcs101@gmail.com>
2025-07-30 23:00:29 +0700

    hash-object + cat-file

--- ugit/cli.py
+++ ugit/cli.py
@@ -3,12 +3,24 @@
 import argparse
 # Python built-in module for parsing command-line arguments.
 import os
+import sys
 
 from . import data
 
 def main():
     args = parse_args()
+    # Parse the command-line arguments.
+    # For example, if the user runs 
+    #   'ugit hash-object example.txt'
+    # parse_args should returns:
+    #   Namespace(
+    #       command='hash-object',
+    #       file='example.txt',
+    #       func=<function hash-object at 0x...>).
+    
     args.func(args)
+    # And this should be equivalent to calling:
+    #   hash_object(args)
     
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -20,6 +32,16 @@ def parse_args():
     # and make the command argument required.
     
     commands.add_parser('init').set_defaults(func=init)
+    # 'ugit init' command
+    
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+    # 'ugit hash-object <file>' command
+    
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
     
     return parser.parse_args()
     # This should return Namespace(command='init', func=<function 'init' below>) for 'ugit init'
@@ -28,3 +50,15 @@ def parse_args():
 def init(args):
     data.init()
     print(f'Initialized ugit repository in {os.getcwd()}/{data.GIT_DIR}')
+    
+def hash_object(args):
+    with open (args.file, 'rb') as f: # 'rb' for reading binary files
+        print(data.hash_object(f.read()))
+        
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
+    
+    # Here we use sys.stdout.buffer to write binary data directly to stdout.
+    # Before that, we flush stdout to ensure that any buffered output is written immediately.
+    # For example, if before this line there was a print statement, it would be flushed before writing the binary data.
--- ugit/data.py
+++ ugit/data.py
@@ -1,5 +1,6 @@
 # This file manages the data in .ugit directory. here will be the code that actually touches files on disk.add()
 
+import hashlib
 import os
 
 GIT_DIR = '.ugit'
@@ -7,6 +8,17 @@ GIT_DIR = '.ugit'
 def init():
     if not os.path.exists(GIT_DIR): # Check directory existence in cwd
         os.makedirs(GIT_DIR)
+        os.makedirs(os.path.join(GIT_DIR, 'objects'))
         print(f"Created directory: {GIT_DIR}")
     else:
         print(f"Directory {GIT_DIR} already exists.")
+        
+def hash_object(data): # data should be in bytes (e.g. b'hello world' -> binary)
+    oid = hashlib.sha256(data).hexdigest() # dont forget to digest the sha256 HASH object
+    with open(os.path.join(GIT_DIR, 'objects', oid), 'wb') as out: # 'wb' for binary write
+        out.write(data)
+    return oid
+
+def get_object(oid):
+    with open (os.path.join(GIT_DIR, 'objects', oid), 'rb') as f: # 'rb' for binary read
+        return f.read()
