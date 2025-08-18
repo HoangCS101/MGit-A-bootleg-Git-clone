e441268f5d46040aba6a74be3b4257fb8d8028d9
HoangCS101 <hoangcs101@gmail.com>
2025-08-18 23:53:21 +0700

    update log

--- ugit/cli.py
+++ ugit/cli.py
@@ -116,11 +116,27 @@ def commit(args):
     print(base.commit(args.message))
     
 def log(args):
+    refs = {}
+    for refname, ref in data.iter_refs():
+        refs.setdefault(ref.value, []).append(refname)
+        # for example:
+        # data = [
+        #     ("refs/heads/main", Ref("abc123")),
+        #     ("refs/heads/dev", Ref("abc123")),
+        #     ("refs/tags/v1.0", Ref("def456")),
+        # ]
+        # then:
+        # refs = {
+        #     "abc123": ["refs/heads/main", "refs/heads/dev"],
+        #     "def456": ["refs/tags/v1.0"]
+        # }
+        
     for oid in base.iter_commits_and_parents({args.oid}):
         # {args.oid} creates a set with a single element, args.oid
         # or else python would treat args.oid as a set of separated characters
         commit = base.get_commit(oid)
-        print(f'commit {oid}\n')
+        refs_str = f'({", ".join(refs[oid])})' if oid in refs else ''
+        print(f'commit {oid}{refs_str}\n')
         print(textwrap.indent(commit.message, '    '))
         print('')
         
