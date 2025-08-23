e13a12ee37b985119f0a4977776718fa73fb8861
HoangCS101 <hoangcs101@gmail.com>
2025-08-23 12:36:11 +0700

    show now can compare files (amazing)

--- ugit/cli.py
+++ ugit/cli.py
@@ -166,7 +166,8 @@ def show(args):
     result = diff.diff_trees(
         base.get_tree(parent_tree), base.get_tree(commit.tree)
     )
-    print(result)
+    sys.stdout.flush()
+    sys.stdout.buffer.write(result)
     
 def checkout(args):
     base.checkout(args.commit)
--- ugit/diff.py
+++ ugit/diff.py
@@ -1,6 +1,14 @@
 # codes for dealing with computing differences between trees
 
+import subprocess
+# to spawn new processes, connect to their input/output/error pipes, and obtain their return codes
+
 from collections import defaultdict
+from tempfile import NamedTemporaryFile as Temp
+# for creating named temporary file, which can be accessed via real file's name
+
+
+from . import data
 
 def compare_trees(*trees): # allow n tree (diff_trees passes 2)
     entries = defaultdict(lambda: [None] * len(trees))
@@ -50,8 +58,37 @@ def diff_trees(t_from, t_to): # prev tree vs next tree
     # }
     # from base.get_tree()
     
-    output =''
+    output = b''
     for path, o_from, o_to in compare_trees(t_from, t_to):
         if o_from != o_to:
-            output += f'changed: {path}\n'
+            output += diff_blobs(o_from, o_to, path)
     return output
+
+def diff_blobs(o_from, o_to, path='blob'):
+    with Temp() as f_from, Temp() as f_to:
+    # create 2 temp files as 'f_from' and 'f_to', existing only within 'with'
+    # temp files are needed since raw binary files in .ugit/objects cant be compared by 'diff'
+        for oid, f in ((o_from, f_from), (o_to, f_to)):
+            if oid:
+                f.write(data.get_object(oid))
+                f.flush()
+                # normally python will automatically flush the buffer when:
+                # closed() -> close file
+                # out of 'with'
+                # buffer overflow
+                # here files content is needed within another process
+                # which is calling 'diff' using 'subprocess'
+                
+        with subprocess.Popen( # Process open
+            ['diff', '--unified', '--show-c-function',
+             '--label', f'a/{path}', f_from.name,
+             '--label', f'b/{path}', f_to.name],
+            stdout=subprocess.PIPE) as proc:
+            # stdout=subprocess.PIPE request result to be piped into python (not printed to terminal)
+            # 'with ... as proc': context manager is used to make sure 'proc' is release when out of 'with'
+            output, _ = proc.communicate()
+            # interact with process, here send no stdin (input)
+            # read data from stdout (output) and stderr (ignored)
+            # until end-of-file is reached. Wait for process to terminate.
+
+        return output
